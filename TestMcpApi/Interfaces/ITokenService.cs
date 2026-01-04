f8859f41b570e2ddecbc840a5018a68e08c941f3
rodaina <rodaina@itabula.com>
2026-01-04 22:03:57 +0200

    token service

---
+++ TestMcpApi/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestMcpApi.Interfaces
+{
+    public interface ITokenService
+    {
+
+        string GenerateJwtToken(string username, string userId, List<string> UserFeatures);
+        public string GenerateRefreshToken();
+        public Task<IActionResult> RefreshToken(string JwtToken, string RefreshToken);
+
+    }
+}
