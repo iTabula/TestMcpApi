58d628f515a53e6a5bc076e3dd401aacdc51a852
rodaina <rodaina@itabula.com>
2026-01-11 17:49:52 +0200

    adding generic repository

---
+++ TestMcpApi/Services/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestMcpApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMcpApi.Services
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T?> GetById(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task Add(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<T?> GetByCredentials(string username, string password)
+        {
+            return await _dbSet.FirstOrDefaultAsync();
+        }
+
+        public async Task<T?> GetGroupByUserId(int Id)
+        {
+            return await _dbSet.FirstOrDefaultAsync();
+        }
+    }
+}
