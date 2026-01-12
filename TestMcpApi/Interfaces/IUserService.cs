41fba90b83bceff99ae25fe7ce57fb6bc0b12cf4
rodaina <rodaina@itabula.com>
2026-01-12 15:50:47 +0200

    user service

--- TestMcpApi/Interfaces/IUserService.cs
+++ TestMcpApi/Interfaces/IUserService.cs
@@ -7,15 +7,12 @@ namespace TestMcpApi.Services
 {
     public interface IUserService
     {
-        string ErrorLoadCsv { get; }
-
-        Task<List<User>> GetUsers(string AccessToken);
-        Task<User> GetUserById(string AccessToken, string UserId);
+        Task<List<UserPartial>> GetUsers(string AccessToken);
+        Task<List<User>> GetUsersBelongToGroup(string AccessToken, string groupId);
+        Task<UserPartial> GetUser(string AccessToken, string UserId);
         Task<User> GetUserByEmail(string AccessToken, string Email);
-
-        //Task<LoginResponse> LoginUser(string UserName, string Password);
-        //Task<User> CreateUser(string AccessToken, User user);
-        //Task<User> UpdateUser(string AccessToken, User user, int UserId);
-        //Task<List<User>> GetUsersBelongToGroup(string AccessToken, string groupId);
+        Task<LoginResponse> LoginUser(string UserName, string Password);
+        Task<User> CreateUser(string AccessToken, User user);
+        Task<User> UpdateUser(string AccessToken, User user, int UserId);
     }
 }
--- TestMcpApi/Services/UserService.cs
+++ TestMcpApi/Services/UserService.cs
@@ -1,68 +1,97 @@
-using System.Net.Http.Json;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using Microsoft.Extensions.Http;
 using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Http;
+using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using TestMcpApi.Interfaces;
 using TestMcpApi.Models;
 
 namespace TestMcpApi.Services;
 
 public class UserService : IUserService
 {
-    private readonly List<User> _data = new();
-    private string _errorLoadCsv = string.Empty;
-
-    public string ErrorLoadCsv => _errorLoadCsv;
-
-    public UserService()
+    private readonly IFactoryHttpClient _factoryHttpClient;
+    private readonly IConfiguration _configuration;
+    private readonly string _WebApiName = string.Empty;
+    public UserService(IConfiguration configuration, IFactoryHttpClient factoryHttpClient)
     {
-        _errorLoadCsv = LoadData();
-    }
-    private string LoadData()
-    {
-        try
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
-
-            var configuration = builder.Build();
-            var connStr = configuration.GetConnectionString("DefaultConnection");
-            if (string.IsNullOrEmpty(connStr))
-                return "Connection string 'DefaultConnection' not found in appsettings.json";
-
-            using SqlConnection db = new SqlConnection(connStr);
-            const string sql = "SELECT * FROM Users ORDER BY 1 DESC";
-            var list = db.Query<User>(sql).AsList();
-            _data.AddRange(list);
-
-            return string.Empty;
-        }
-        catch (Exception ex)
-        {
-            return $"Error loading data from database: {ex.Message}";
-        }
+        _factoryHttpClient = factoryHttpClient;
+        _configuration = configuration;
+        _WebApiName = "MyWebApi";
     }
 
-    public Task<List<User>> GetUsers(string AccessToken)
-        => Task.FromResult(_data);
+    public async Task<List<UserPartial>> GetUsers(string AccessToken)
+    {
+        List<UserPartial> data = await _factoryHttpClient.GetRequest<List<UserPartial>>(_WebApiName,
+            $"api/Users", null!, AccessToken);
 
-    public Task<User?> GetUserById(string AccessToken, string userId)
-        => Task.FromResult(_data.FirstOrDefault(l => l.UserId.ToString() == userId));
+        return data;
+    }
+    public async Task<User> GetUserByEmail(string AccessToken, string Email)
+    {
+        User data = await _factoryHttpClient.GetRequest<User>(_WebApiName,
+            $"api/Users/Email/{Email}", null!, AccessToken);
+        return data;
+    }
 
-    public Task<User?> GetUserByEmail(string AccessToken, string email)
-        => Task.FromResult(_data.FirstOrDefault(l => l.Email == email));
+    public async Task<List<User>> GetUsersBelongToGroup(string AccessToken, string groupId)
+    {
 
-    //public Task<LoginResponse> LoginUser(string UserName, string Password)
-    //{
+        List<User> result = await _factoryHttpClient.GetRequest<List<User>>(_WebApiName,
+            $"api/Users/Group/{groupId}", null!, AccessToken);
 
-    //}
+        return result;
+    }
+
+    public async Task<UserPartial> GetUser(string AccessToken, string UserId)
+    {
+
+        UserPartial result = await _factoryHttpClient.GetRequest<UserPartial>(_WebApiName,
+            $"api/Users/{UserId}", null!, AccessToken);
+
+        return result;
+    }
+
+    public async Task<User> UpdateUser(string AccessToken, User user, int UserId)
+    {
+        HttpContent httpContent = _factoryHttpClient.SetHttpContent(user);
+
+        User result = await _factoryHttpClient.PostRequest<User>(_WebApiName,
+            $"api/Users/Update/{UserId}", httpContent!, AccessToken);
+
+        return result;
+    }
+
+
+    public async Task<User> CreateUser(string AccessToken, User user)
+    {
+        HttpContent httpContent = _factoryHttpClient.SetHttpContent(user);
+
+        User result = await _factoryHttpClient.PostRequest<User>(_WebApiName,
+            $"api/Users/Create", httpContent!, AccessToken);
+
+        return result;
+    }
+
+    public async Task<LoginResponse> LoginUser(string UserName, string Password)
+    {
+        LoginRequest loginRequest = new LoginRequest()
+        {
+            UserName = UserName,
+            Password = Password
+        };
+        HttpContent httpContent = _factoryHttpClient.SetHttpContent(loginRequest);
+        LoginResponse data = await _factoryHttpClient.PostRequest<LoginResponse>(_WebApiName,
+            $"api/Users/login", httpContent, "");
+
+        return data;
+    }
 
 }
 
