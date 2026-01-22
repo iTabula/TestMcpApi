056761b005b31b2a477bce8304ff4007422e73f5
KHALED EL HENAWY <khenawy@gmail.com>
2026-01-22 17:34:07 +0000

    changes ot Web API to make it publishable

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi;
 using System.Text;
 using System.Text.Json.Serialization;
 using KamInfrastructure.DBContexts;
@@ -86,39 +85,13 @@ builder.Services.AddAuthentication(x =>
 
 builder.Services.AddAuthorization();
 
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "KAM", Version = "v1" });
-//    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-
-//    var securityScheme = new OpenApiSecurityScheme
-//    {
-//        Name = "JWT Authentication",
-//        Description = "Enter JWT Bearer token **_only_**",
-//        In = ParameterLocation.Header,
-//        Type = SecuritySchemeType.Http,
-//        Scheme = "bearer", // must be lower case
-//        BearerFormat = "JWT",
-//        Reference = new OpenApiReference
-//        {
-//            Id = JwtBearerDefaults.AuthenticationScheme,
-//            Type = ReferenceType.SecurityScheme
-//        }
-//    };
-//    c.AddSecurityDefinition(securityScheme.Reference.Id, securityScheme);
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//                {
-//                    {securityScheme, new string[] { }}
-//                });
-//});
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
